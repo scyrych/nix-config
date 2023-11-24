60e224c506feb8d848ddf194f135da94e8b26962
Sebastian Cyrych <s.cyrych@gmail.com>
2023-11-24 11:26:16 +0100

    alacritty update

--- modules/home-manager/alacritty/default.nix
+++ modules/home-manager/alacritty/default.nix
@@ -3,6 +3,7 @@
 {
   programs.alacritty = {
     enable = true;
+    package = pkgs.unstable.alacritty;
   };
   xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
   xdg.configFile."alacritty/catppuccin-mocha.yml".source = ./catppuccin-mocha.yml;
